d1a3535c7723676888b7ca111653cf933c889110
Roger Granada <roger.leitzke@gmail.com>
2019-08-22 03:30:57 -0300

    Detect bounding boxes for a person in KSGCR dataset

--- Python/skindetection.py
+++ Python/skindetection.py
@@ -1,70 +1,128 @@
+import argparse
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-image = cv2.imread("3346.jpg")
-image = image[:210,:]
-"""
-min_HSV = np.array([0, 58, 30], dtype = "uint8")
-max_HSV = np.array([33, 150, 255], dtype = "uint8")
-# Get pointer to video frames from primary device
+def find_hands_contour(contours, threshold=600):
+    """ Identify contours by size of the area """
+    hands_contours = []
+    for i, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        if (area > threshold):
+            print area
+            hands_contours.append(contours[i])
+    return hands_contours
 
-imageHSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-skinRegionHSV = cv2.inRange(imageHSV, min_HSV, max_HSV)
-#image2 = np.ones(image.shape)*255
 
-mask = cv2.bitwise_and(image, image, mask = skinRegionHSV)
-#cv2.imwrite("283_s.jpg", np.hstack([image, mask]))
-
-contours, hierarchy =  cv2.findContours(image,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-cv2.drawContours(image, contours, -1, (0, 0, 255), 3)
-
-#cv2.imshow("Keypoints for ", im_with_keypoints)
-#cv2.waitKey(0)  
-"""
-def viewImage(image):
-    cv2.namedWindow('Display', cv2.WINDOW_NORMAL)
-    cv2.imshow('Display', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-def findGreatesContour(contours):
+def find_largest_contour(contours):
+    """ Find the largest contour fo the list """
     largest_area = 0
-    largest_contour_index = -1
-    i = 0
-    total_contours = len(contours)
-    while (i < total_contours ):
-        area = cv2.contourArea(contours[i])
+    idx_largest = -1
+    for i, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
         if(area > largest_area):
             largest_area = area
             largest_contour_index = i
-        i+=1
-            
-    return largest_area, largest_contour_index
+    return largest_area, idx_largest
 
 
-hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-green_low = np.array([0, 58, 30] )
-green_high = np.array([33, 150, 255])
-curr_mask = cv2.inRange(hsv_img, green_low, green_high)
-mask = cv2.bitwise_and(image, image, mask = curr_mask)
-#viewImage(mask) ## 2
-## contouring
-RGB_again = cv2.cvtColor(mask, cv2.COLOR_HSV2RGB)
-gray = cv2.cvtColor(RGB_again, cv2.COLOR_RGB2GRAY)
-#viewImage(gray) ## 3
-ret, threshold = cv2.threshold(gray, 75, 105, 0)
-#viewImage(threshold) ## 4
-contours, hierarchy =  cv2.findContours(threshold,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-#cv2.drawContours(image, contours, -1, (0, 0, 255), 3)
-#viewImage(image) ## 5
+def find_bounding_box(list_contours):
+    """ Find minimum and maximum points for a single bbox """
+    min_x, max_x = 256, 0 
+    min_y, max_y = 256, 0
+    for contour in list_contours:
+        for points in contour:
+            for x, y in points:
+                if x < min_x: min_x = x
+                if x > max_x: max_x = x
+                if y < min_y: min_y = y
+                if y > max_y: max_y = y
+    return min_x, max_x, min_y, max_y 
 
-max_contours = []
-largest_area, largest_contour_index = findGreatesContour(contours)
-max_contours.append(contours[largest_contour_index])
-del contours[largest_contour_index]
-largest_area, largest_contour_index = findGreatesContour(contours)
-max_contours.append(contours[largest_contour_index])
-cv2.drawContours(image, max_contours, -1, (0, 0, 255), 3)
-viewImage(image)
 
+def convert_xy_bbox(xmin, xmax, ymin, ymax):
+    """ Convert x and y coordinates to x, y, w, and h """
+    x, y = xmin-5, ymin-5
+    w, h = xmax-xmin+10, ymax-ymin+10
+    return x, y, w, h
+
+
+def draw_box(image, bbox):
+    """ Draw a bounding box in the input image """
+    from PIL import Image
+    import matplotlib.pyplot as plt
+    import matplotlib.patches as patches
+    import numpy as np
+    im = np.array(Image.open(image), dtype=np.uint8)
+    fig, ax = plt.subplots(1)
+    ax.imshow(im)
+    for x, y, w, h in bbox:
+        rect = patches.Rectangle((x, y), w, h, linewidth=1, edgecolor='r', facecolor='none')
+        ax.add_patch(rect)
+    plt.show()
+    
+
+def detect_skin(image):
+    """ Detect hands by skin color """
+    image = image[:210,:]
+    hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    skin_low = np.array([0, 58, 30] )
+    skin_high = np.array([33, 150, 255])
+    mask = cv2.inRange(hsv_img, skin_low, skin_high)
+    mask = cv2.bitwise_and(image, image, mask=mask)
+
+    rgb_mask = cv2.cvtColor(mask, cv2.COLOR_HSV2RGB)
+    gray = cv2.cvtColor(rgb_mask, cv2.COLOR_RGB2GRAY)
+    ret, threshold = cv2.threshold(gray, 75, 105, 0)
+    contours, hierarchy = cv2.findContours(threshold, 
+                                           cv2.RETR_TREE, 
+                                           cv2.CHAIN_APPROX_SIMPLE)
+    hands = find_hands_contour(contours, threshold=600)
+    if hands:
+        xmin, xmax, ymin, ymax = find_bounding_box(hands)
+    return xmin, xmax, ymin, ymax
+
+
+def detect_person(image):
+    """ Detect person by black t-shirt """
+    image = image[:10,:]
+    hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    person_low = np.array([0,0,100] )
+    person_high = np.array([255,255,255])
+    mask = cv2.inRange(hsv_img, person_low, person_high)
+    mask = cv2.bitwise_and(image, image, mask=mask)
+    mask = 255 - mask
+    mask = np.uint8(np.where(mask > 200, 255, 0))
+    rgb_mask = cv2.cvtColor(mask, cv2.COLOR_HSV2RGB)
+    gray = cv2.cvtColor(rgb_mask, cv2.COLOR_RGB2GRAY)
+    contours, hierarchy =  cv2.findContours(gray, 
+                                            cv2.RETR_TREE, 
+                                            cv2.CHAIN_APPROX_SIMPLE)
+    largest_area, largest_contour_index = find_largest_contour(contours)
+    xmin, xmax, ymin, ymax = 0, 0, 0, 0
+    if largest_area:
+        person = contours[largest_contour_index]
+        xmin, xmax, _, _ = find_bounding_box([person])
+        ymin, ymax = 0, 105
+    return xmin, xmax, ymin, ymax
+
+
+def detect_bbox_person(input_image):
+    """ Given an image, detect the person and his hands to draw a box """
+    image = cv2.imread(input_image)
+    xmin_h, xmax_h, ymin_h, ymax_h = detect_skin(image)
+    xmin_p, xmax_p, ymin_p, ymax_p = detect_person(image)
+    xmin = xmin_h if (xmin_h < xmin_p) else xmin_p
+    xmax = xmax_h if (xmax_h > xmax_p) else xmax_p
+    ymin = ymin_h if (ymin_h < ymin_p) else ymin_p
+    ymax = ymax_h if (ymax_h > ymax_p) else ymax_p
+    x, y, w, h = convert_xy_bbox(xmin, xmax, ymin, ymax)
+    draw_box(input_image, [[x, y, w, h]])
+    return x, y, w, h
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('imgfile', metavar='image', help='Path to the image')
+    args = argparser.parse_args()
+    detect_bbox_person(args.imgfile)
