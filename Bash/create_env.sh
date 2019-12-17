5cb11c40c489eaedcccd72eb18d1585429f23958
roger <roger.leitzke@gmail.com>
2019-12-17 10:31:26 -0300

    Create conda environment

---
+++ Bash/create_env.sh
@@ -0,0 +1,12 @@
+# Create env
+conda create -n yourenvname python=x.x anaconda
+
+# Install package
+conda install -n yourenvname [package]
+
+# Activate env
+source activate yourenvname
+
+# Deactivate env
+source deactivate
+conda deactivate
